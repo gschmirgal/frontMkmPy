2753c31c527a49f865c519edbfd732a57e549804
gschmirgal <g.schmirgal@gmail.com>
2025-08-07 18:35:02 +0200

    proto des vues et des controllers

---
+++ src/Controller/CardController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\ExpansionsRepository;
+use App\Repository\ProductsRepository;
+use App\Repository\PricesRepository;
+
+class CardController extends AbstractController{
+
+    #[Route('/cards', name: 'card.list')]
+    function show (Request $request, ProductsRepository $repository ): Response 
+    {
+        $cards = $repository->findAll();
+        return $this->render('cardList.html.twig', [
+            'cards' => $cards,
+        ]);
+    }
+
+    #[Route('/card/{id}', name: 'card.expansionlist', requirements: ['id' => '\d+'])]
+    function showCardsExpansion (Request $request, int $id, ExpansionsRepository $repositoryExp, productsRepository $repositoryProd ): Response 
+    {   
+        $originalcard = $repositoryProd->find($id);
+        $cards = $repositoryProd->findBy(['idMetaCard' => $originalcard->getIdMetaCard()]);
+        return $this->render('cardExpansionList.html.twig', [
+            'originalcard' => $originalcard,
+            'cards' => $cards,
+        ]);
+    }
+
+    #[Route('/card/{cardid}/{expansionid}/', name: 'card.cardlist.detail', requirements: ['expansionid' => '\d+', 'cardid' => '\d+'])]
+    function showSpecificCardExpansion (Request $request, int $expansionid, int $cardid ): Response 
+    {   
+        return $this->redirectToRoute('expansion.cardlist.detail', [
+            'expansionid' => $expansionid,
+            'cardid' => $cardid,
+        ]);
+    }
+
+}
---
+++ src/Controller/ExpansionController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+use App\Repository\ExpansionsRepository;
+use App\Repository\ProductsRepository;
+use App\Repository\PricesRepository;
+
+class ExpansionController extends AbstractController{
+
+    #[Route('/expansions', name: 'expansion.list')]
+    function show (Request $request, ExpansionsRepository $repository ): Response 
+    {
+        $expansions = $repository->findAll();
+        return $this->render('expansionList.html.twig', [
+            'expansions' => $expansions,
+        ]);
+    }
+
+    #[Route('/expansion/{id}', name: 'expansion.cardlist', requirements: ['id' => '\d+'])]
+    function showCardsExpansion (Request $request, int $id, ExpansionsRepository $repositoryExp, productsRepository $repositoryProd ): Response 
+    {   
+        $expansion = $repositoryExp->find($id);
+        $cards = $repositoryProd->findBy(['expansion' => $id]);
+        return $this->render('expansionCardList.html.twig', [
+            'expansion' => $expansion,
+            'cards' => $cards,
+        ]);
+    }
+
+    #[Route('/expansion/{expansionid}/{cardid}', name: 'expansion.cardlist.detail', requirements: ['expansionid' => '\d+', 'cardid' => '\d+'])]
+    function showSpecificCardExpansion (Request $request, int $expansionid, int $cardid, ExpansionsRepository $repositoryExp, ProductsRepository $repositoryProd, PricesRepository $repositoryPrices ): Response 
+    {   
+        $expansion = $repositoryExp->find($expansionid);
+        $card = $repositoryProd->find($cardid);
+        $prices = $repositoryPrices->findBy(['product' => $cardid]);
+        //dd($expansion, $card, $prices);
+
+        $tableColumns = [
+            'dateData'  => 'Date',
+            'avg'       => 'moyenne',
+            'low'       => 'plus bas',
+            'trend'     => 'tendance',
+            'avg1'      => 'moyenne 1 jour',
+            'avg7'      => 'moyenne 7 jours',
+            'avg30'     => 'moyenne 30 jours',
+            'avgFoil'   => 'moyenne foil',
+            'lowFoil'   => 'plus bas foil',
+            'trendFoil' => 'tendance foil',
+            'avg1Foil'  => 'moyenne 1 jour foil',
+            'avg7Foil'  => 'moyenne 7 jours foil',
+            'avg30Foil' => 'moyenne 30 jours foil',
+
+        ];
+
+
+        return $this->render('expansionCardListDetail.html.twig', [
+            'expansion' => $expansion,
+            'card' => $card,
+            'prices' => $prices,
+            'tableColumns' => $tableColumns,
+        ]);
+    }
+
+}
