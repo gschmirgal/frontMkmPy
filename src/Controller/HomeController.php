0fce2bdbb67f7e99815a7cdc5d4f664b2f465554
gschmirgal <g.schmirgal@gmail.com>
2025-08-07 18:55:54 +0200

    update controller/views

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\ExpansionsRepository;
+use App\Repository\ProductsRepository;
+use App\Repository\PricesRepository;
+
+class HomeController extends AbstractController{
+
+    #[Route('/', name: 'home')]
+    function home (Request $request): Response 
+    {
+        return $this->render('home.html.twig');
+    }
+
+}
